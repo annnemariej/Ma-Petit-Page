9971a0c426573446e895775902e2f1e8736bea08
annnemariej <anne-marie@meireles.no>
2025-05-12 13:56:17 +0200

    laget ferdig(uten at den ser fint ut) nav baren

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const dropdownContent = document.querySelector(".dropdown-content")
 
-function showDropdownContent() {
-    dropdownContent.style.display = "flex"
-    dropdownContent.style.flexDirection = "column-reverse"
+///NAV BAR
+const burger = document.getElementById("burger");
+const dropdown = document.getElementById("dropdown-content");
 
-}
+burger.addEventListener("mouseenter", () => {
+    dropdown.classList.add("show");
+});
 
-function hideDropDown() {
-    dropdownContent.style.display = "none"
+burger.addEventListener("mouseleave", () => {
+    dropdown.classList.remove("show");
+});
 
+
+//Darkmode
+
+let light = localStorage.getItem("light") === "true" ? true : false; 
+document.body.classList.add(light ? "light" : "dark");
+function darkModeLightMode() {
+    if (light) {
+        console.log("det er light mode")
+        light = false;
+        localStorage.setItem("light", false)
+        document.body.classList.remove("light")
+        document.body.classList.add("dark")
+    } else {
+        console.log("det er darkmode")
+        light = true;
+        localStorage.setItem("light", true)
+        document.body.classList.remove("dark")
+        document.body.classList.add("light")
+    }
 }
