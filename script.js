d5ef6c18325d6807d40929d28bd6ab847c31fe0f
Maggie1111111 <margrethe.koller@outlook.com>
2025-05-25 23:28:38 +0200

    gsap animasjon på les mer knapp

--- script.js
+++ script.js
@@ -317,25 +317,118 @@ fetch(`https://gnews.io/api/v4/top-headlines?topic=technology&lang=no&max=10&tok
             <img src="${article.image || ''}" alt="Bilde">
             <h3>${article.title}</h3>
             <p>${article.description || ''}</p>
-            <a href="${article.url}" target="_blank">Les mer</a>
+            <a href="${article.url}" class="button button--stroke" data-block="button" target="_blank">
+              <span class="button__flair"></span>
+              <span class="button__label">Les Mer</span>
+            </a>
           `
       nyhet.appendChild(card);
-    });
+    })
+    const buttonElements = document.querySelectorAll('[data-block="button"]');
+
+  buttonElements.forEach((buttonElement) => {
+    new Button(buttonElement);
   })
+  })
+
   .catch(err => {
     console.error(err)
     nyhet.innerHTML = '<p>Kunne ikke hente nyheter.</p>'
-  });
+  })
 
-const nextKnapp = document.getElementById('nextKnapp');
-const prevKnapp = document.getElementById('prevKnapp');
+const nextKnapp = document.getElementById('nextKnapp')
+const prevKnapp = document.getElementById('prevKnapp')
 
 nextKnapp.addEventListener('click', () => {
   nyhet.scrollBy({ left: 320, behavior: 'smooth' })
-});
+})
 
 prevKnapp.addEventListener('click', () => {
   nyhet.scrollBy({ left: -320, behavior: 'smooth' })
-});
+})
 
+class Button {
+  constructor(buttonElement) {
+    this.block = buttonElement
+    this.init()
+    this.initEvents()
+  }
+
+  init() {
+    const el = gsap.utils.selector(this.block)
+
+    this.DOM = {
+      button: this.block,
+      flair: el(".button__flair")
+    }
+
+    this.xSet = gsap.quickSetter(this.DOM.flair, "xPercent")
+    this.ySet = gsap.quickSetter(this.DOM.flair, "yPercent")
+  }
+
+  getXY(e) {
+    const {
+      left,
+      top,
+      width,
+      height
+    } = this.DOM.button.getBoundingClientRect()
+
+    const xTransformer = gsap.utils.pipe(
+      gsap.utils.mapRange(0, width, 0, 100),
+      gsap.utils.clamp(0, 100)
+    )
+
+    const yTransformer = gsap.utils.pipe(
+      gsap.utils.mapRange(0, height, 0, 100),
+      gsap.utils.clamp(0, 100)
+    )
+
+    return {
+      x: xTransformer(e.clientX - left),
+      y: yTransformer(e.clientY - top)
+    }
+  }
+
+  initEvents() {
+    this.DOM.button.addEventListener("mouseenter", (e) => {
+      console.log("HOVER!")
+      const { x, y } = this.getXY(e)
+
+      this.xSet(x)
+      this.ySet(y)
+
+      gsap.to(this.DOM.flair, {
+        scale: 1,
+        duration: 0.4,
+        ease: "power2.out"
+      })
+    })
+
+    this.DOM.button.addEventListener("mouseleave", (e) => {
+      const { x, y } = this.getXY(e)
+
+      gsap.killTweensOf(this.DOM.flair)
+
+      gsap.to(this.DOM.flair, {
+        xPercent: x > 90 ? x + 20 : x < 10 ? x - 20 : x,
+        yPercent: y > 90 ? y + 20 : y < 10 ? y - 20 : y,
+        scale: 0,
+        duration: 0.3,
+        ease: "power2.out"
+      })
+    })
+
+    this.DOM.button.addEventListener("mousemove", (e) => {
+      const { x, y } = this.getXY(e)
+
+      gsap.to(this.DOM.flair, {
+        xPercent: x,
+        yPercent: y,
+        duration: 0.4,
+        ease: "power2"
+      })
+    })
+  }
+}
 
