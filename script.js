d04aa9cf78a2da0c97c6e5068e893449c9576134
Maggie1111111 <margrethe.koller@outlook.com>
2025-06-03 12:00:21 +0200

    Smooth scroll!!!

--- script.js
+++ script.js
@@ -591,3 +591,25 @@ class Button {
     }
 }
 
+//smooth scroll
+function smoothScroll(target) {
+    const element = document.getElementById(target);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const anchors = document.querySelectorAll('a[href^="#"]')
+    anchors.forEach(function(anchor) {
+      anchor.addEventListener('click', function(event) {
+        event.preventDefault()
+        const target = anchor.getAttribute('href').substring(1)
+        smoothScroll(target)
+        console.log("du trykket på knappen")
+      })
+    })
+  })
