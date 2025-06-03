fd05a6e56974958652fe6befcccc491a3b612847
Maggie1111111 <margrethe.koller@outlook.com>
2025-06-03 14:55:29 +0200

    fjernet section 2 og 3 timeplan, litt rydding

--- script.js
+++ script.js
@@ -593,7 +593,7 @@ class Button {
 
 //smooth scroll
 function smoothScroll(target) {
-    const element = document.getElementById(target);
+    const element = document.getElementById(target)
     if (element) {
       window.scrollTo({
         top: element.offsetTop,
